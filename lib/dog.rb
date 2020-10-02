9bac72758c6bc324667add6bff6a1603c8d3946b
Plimon Howard V <plimon.howard@gmail.com>
2020-10-02 20:48:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 class Dog
   def initialize(name,breed)
     @name = name
-    @breed = breed
+    @breed = "Mutt"
   end
 
   def breed=(breed)
-    @breed = "Mutt"
+    @breed = breed
   end
   def breed
     @breed
   end
+  
 end
